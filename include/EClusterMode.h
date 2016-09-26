61ebd0c9a31c8436518b9fee637d17794a0966db
Andre Sailer <andre.philippe.sailer@cern.ch>
2016-09-26 11:15:57 +0200

    Move clustermode ostream operator to header file to fix undefined reference when including

--- include/EClusterMode.h
+++ include/EClusterMode.h
@@ -1,6 +1,9 @@
 #ifndef EClusterMode_h
 #define EClusterMode_h 1
 
+#include <iostream>
+
+
 // The enum, name and value of the enum for the Cluster Mode
 enum EClusterMode {
 	NONE = 0,
@@ -9,5 +12,22 @@ enum EClusterMode {
 	FJ_inclusive = 4,		// inclusive "-"
 	OWN_inclusiveIteration = 8	// use FJ inclusive Clustering, but iterate until we have the desired number of jets
 };
+
+std::ostream& operator<<(std::ostream& out, EClusterMode& m) {
+
+  switch (m) {
+  case OWN_inclusiveIteration:
+    out << "InclusiveIterativeNJets"; break;
+  case FJ_inclusive:
+    out << "Inclusive"; break;
+  case FJ_exclusive_nJets:
+    out << "ExclusiveNJets"; break;
+  case FJ_exclusive_yCut:
+    out << "ExclusiveYCut"; break;
+  default:
+    out << "unknown"; break;
+  }
+  return out;
+}
 
 #endif // EClusterMode_h
--- src/FastJetProcessor.cpp
+++ src/FastJetProcessor.cpp
@@ -96,22 +96,6 @@ void FastJetProcessor::init()
 
 }
 
-
-
-std::ostream& operator<<(std::ostream& out, EClusterMode& m)
-{
-  switch (m)
-    {
-    case OWN_inclusiveIteration:	out << "InclusiveIterativeNJets"; break;
-    case FJ_inclusive:				out << "Inclusive"; break;
-    case FJ_exclusive_nJets:		out << "ExclusiveNJets"; break;
-    case FJ_exclusive_yCut:			out << "ExclusiveYCut"; break;
-    default:						out << "unknown"; break;
-    }
-
-  return out;
-}
-
 /** Called for every event - the working horse.
  */
 void FastJetProcessor::processEvent(LCEvent * evt)
