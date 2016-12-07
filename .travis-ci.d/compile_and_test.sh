1ba3a8e69fc610872dbd521aec99927201532cd7
Marko Petric <marko.petric@cern.ch>
2016-12-07 14:15:45 +0100

    Add LLVM CI

--- .travis-ci.d/compile_and_test.sh
+++ .travis-ci.d/compile_and_test.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-export GCC_VERSION="4.8.5"
-export BUILD_TYPE="opt"
+ILCSOFT=/cvmfs/clicdp.cern.ch/iLCSoft/builds/current/CI_${COMPILER}
+source $ILCSOFT/init_ilcsoft.sh
 
-source /MarlinFastJet/.travis-ci.d/init_x86_64.sh
-
-cd /MarlinFastJet
+cd /Package
 mkdir build
 cd build
 cmake -GNinja -C $ILCSOFT/ILCSoft.cmake ..
 ninja
-
+ctest --output-on-failure
